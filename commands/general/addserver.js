0f4b8c2bd1f1c6535ab9df754337e7b2c045cf42
dftd <lil.funk.133@gmail.com>
2020-10-30 20:08:12 +0100

    Host validation

--- commands/general/addserver.js
+++ commands/general/addserver.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const isValidDomain = require('is-valid-domain');
+const isValidIP = require('is-valid-ip');
+const isValidPort = (port => (port >= 0 && port <= 65535));
 
 module.exports = {
     name: 'addserver',
@@ -14,7 +17,7 @@ module.exports = {
 
 module.exports.execute = async(bot, msg, args, data) => {
     let game = args[0];
-    let ip = args[1];
+    let host = args[1];
     let name = args.splice(2).join(' ').trim();
     let prefix = !data.guild.prefix ? bot.config.prefix : data.guild.prefix;
 
@@ -22,32 +25,30 @@ module.exports.execute = async(bot, msg, args, data) => {
         return bot.embeds.cmdError(msg, 'Game can\'t be empty.', module.exports);
     if(!(game in bot.data.games))
         return bot.embeds.error(msg, 'Invalid game.\nUse `' + prefix + 'games` to see available games.');
-    if(ip === undefined)
+    if(host === undefined)
         return bot.embeds.cmdError(msg, 'Host can\'t be empty.', module.exports);
+
+    let hostSplit = host.split(':');
+    if(hostSplit[1] === undefined) port = null;
+    else { host = hostSplit[0]; port = hostSplit[1]; }
+
+    if((!isValidIP(host) && !isValidDomain(host)) || (port != null && !isValidPort(port)))
+        return bot.embeds.cmdError(msg, 'Host invalid.', module.exports);
     if(name == '')
         return bot.embeds.cmdError(msg, 'Server name can\'t be empty.', module.exports);
-
-    let hostSplit = ip.split(':');
-    if(hostSplit[1] === undefined) {
-        port = null;
-    } else {
-        ip = hostSplit[0];
-        port = hostSplit[1];
-    }
 
     let serversDB = bot.data.getServerSchema();
     let count = await serversDB.count({ guild: msg.guild.id });
     if(count >= bot.config.serverLimit)
         return bot.embeds.error(msg, 'Current limit of gameservers per guild is `' + bot.config.serverLimit + '`.');
 
-    let server = { guild: msg.guild.id, name: name, game: game, host: ip, port: port };
+    let server = { guild: msg.guild.id, name: name, game: game, host: host, port: port };
     let serverDB = new serversDB(server);
     await serverDB.save().catch(err => {
         bot.logger.error('MongoDB server DB error - ' + err);
         return bot.embeds.dbError(msg);
     });
 
-    // TODO: Put server status on insert, alert if IP needs to be whitelisted.
     let embed = new Discord.MessageEmbed()
         .setColor(bot.config.color)
         .setTitle('Server added')
